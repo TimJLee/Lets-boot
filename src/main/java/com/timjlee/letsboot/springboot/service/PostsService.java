375498350c82bd0ec54e3611b3fdbd13857461e2
TimJLee <tim0409@hotmail.com>
2020-04-09 17:00:47 +0900

    Add: add update/search on posts api & testing complete
    posts api 에 수정/조회 기능 구현
    테스트 코드 작성 & 테스트 pass 완료
    --- 등록/수정/조회 기능 구현 & 테스트 코드 작성 & 테스트 완료(조회 테스트 코드는 없음 -> 웹 콘솔로 테스트 완료)

--- src/main/java/com/timjlee/letsboot/springboot/service/PostsService.java
+++ src/main/java/com/timjlee/letsboot/springboot/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.timjlee.letsboot.springboot.service;
 
+import com.timjlee.letsboot.springboot.domain.posts.Posts;
 import com.timjlee.letsboot.springboot.domain.posts.PostsRepository;
+import com.timjlee.letsboot.springboot.web.dto.PostsResponseDto;
 import com.timjlee.letsboot.springboot.web.dto.PostsSaveRequestDto;
+import com.timjlee.letsboot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new
+                IllegalArgumentException("해당 게시글이 존재하지 않습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new
+                IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/timjlee/letsboot/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.timjlee.letsboot.springboot.web.dto;
+
+import com.timjlee.letsboot.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
